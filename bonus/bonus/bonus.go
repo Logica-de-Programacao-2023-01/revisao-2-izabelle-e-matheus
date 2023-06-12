518525d3a05485fd04f4b7cfa8456c1ff67e38b8
Seiabras <126572587+Seiabras@users.noreply.github.com>
2023-06-11 23:20:43 -0300

    Add files via upload

--- bonus/bonus/bonus.go
+++ bonus/bonus/bonus.go
@@ -1,45 +1,24 @@
 package bonus
 
+import "fmt"
+
 // Você recebe uma lista de caminhos, onde `caminhos[i] = [cidadeAi, cidadeBi]` significa que existe um caminho direto que
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
-func Destino(caminhos [][2]string) (string, bool) {
-	grafo := make(map[string]string)
-
+func Destino(caminhos [][2]string) (string, error) {
+	destinos := make(map[string]int)
 	for _, caminho := range caminhos {
 		origem := caminho[0]
 		destino := caminho[1]
-		grafo[origem] = destino
+		destinos[origem]--
+		destinos[destino]++
 	}
 
-	// Verificar se há um caminho válido
-	for _, caminho := range caminhos {
-		origem := caminho[0]
-		_, ok := grafo[origem]
-		if !ok {
-			return "", false
-		}
-	}
-
-	// Encontrar o destino final percorrendo o grafo
-	origem := caminhos[0][0]
-	for {
-		destino, ok := grafo[origem]
-		if !ok {
-			return "", false
-		}
-
-		if destino == origem {
-			return "", false
-		}
-
-		origem = destino
-
-		_, ok = grafo[origem]
-		if !ok {
-			break
+	for destino, count := range destinos {
+		if count == 1 {
+			return destino, nil
 		}
 	}
 
-	return origem, true
+	return "", fmt.Errorf("nenhum destino final encontrado")
 }
